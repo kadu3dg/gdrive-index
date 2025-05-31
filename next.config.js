e60decfc0ab48bb4393b9502407cbe02d22d5dde
kadu3dg <kadulavinia@gmail.com>
2025-05-31 18:12:59 -0300

    fix: atualiza nome da variável de ambiente para GOOGLE_DRIVE_CREDENTIALS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    GD_SERVICE_B64: process.env.GD_SERVICE_B64,
+    GOOGLE_DRIVE_CREDENTIALS: process.env.GOOGLE_DRIVE_CREDENTIALS,
     ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
     SITE_PASSWORD: process.env.SITE_PASSWORD,
     NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
